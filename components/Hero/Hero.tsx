b11c0fd71f44c9af6cdd1e94f44b50c666e4c277
Maykel Arias Torres <hanibal920915@gmail.com>
2023-01-09 18:21:49 -0300

    Including all red social

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,12 @@
 import {FC} from "react";
 import {GetLocaleStorage} from "../../lib/locale";
 import Link from "next/link";
+import Image from "next/image";
+
+import gmail from "../../assert/image/redes/gmail.svg"
+import github from "../../assert/image/redes/github.svg"
+import twitter from "../../assert/image/redes/twitter.svg"
+import linkedin from "../../assert/image/redes/linkedin.svg"
 
 interface Props {
     locale: string;
@@ -12,8 +18,19 @@ const Hero: FC<Props> = ({locale}) => {
             <div className="flex my-auto flex-col">
                 <h1 className="m-0 text-6xl font-bold">{GetLocaleStorage(locale).title}</h1>
                 <p className="text-center text-lg text-blanco-light">Soy Desarrollador FrontEnd </p>
-                <p className="text-center text-lg text-blanco-light">
-                    Contactarme :{" "}
+                <p className="text-center text-lg text-blanco-light self-end flex flex-row items-center justify-center gap-1">
+                    <Link href="https://github.com/m4yk3ldev" target="_blank" rel="noopener"
+                          className="font-bold text-lg text-center">
+                        <Image src={github} alt="Github" width={30} height={30}/>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/maykel-arias-torres-b827a6136/" target="_blank"
+                          rel="noopener" className="font-bold text-lg text-center">
+                        <Image src={linkedin} alt="Github" width={30} height={30}/>
+                    </Link>
+                    <Link href="https://twitter.com/Maykel920915" target="_blank" rel="noopener"
+                          className="font-bold text-lg text-center">
+                        <Image src={twitter} alt="Twitter" width={30} height={30}/>
+                    </Link>
                     <Link
                         type="email"
                         href="mailto:hanibal920915@gmail.com"
@@ -21,7 +38,7 @@ const Hero: FC<Props> = ({locale}) => {
                         rel="noreferrer"
                         className="font-bold text-lg text-center"
                     >
-                        hanibal920915@gmail.com
+                        <Image src={gmail} alt="Gmail Email" width={30} height={30}/>
                     </Link>
                 </p>
             </div>
--- components/Menu/MenuDesktop.tsx
+++ components/Menu/MenuDesktop.tsx
@@ -8,8 +8,8 @@ export const MenuDesktop: FC<Props> = ({className}) => {
     return (
         <>
             <div className={className}>
-                <p className="text-lg">Home</p>
-                <p className="text-base">Portafolio</p>
+                {/*<p className="text-lg">Home</p>*/}
+                {/*<p className="text-base">Portafolio</p>*/}
             </div>
         </>
     );
