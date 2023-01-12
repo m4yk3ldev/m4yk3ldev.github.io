f6f641694b6462ceb620da586475d00a73997a08
Maykel Arias Torres <hanibal920915@gmail.com>
2023-01-12 10:03:13 -0300

    Adding degraded in background

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,14 +2,17 @@ import {useRouter} from "next/router";
 import {FC} from "react";
 import {Navbar} from "../Navbar/Navbar";
 import Hero from "../Hero/Hero"
+import BackgroudAnimate from "./backgroudAnimate";
 
 const Home: FC = () => {
     const router = useRouter();
     const {locale} = router;
     return (
         <div className="container mx-auto">
+            <BackgroudAnimate />
             <Navbar/>
             <Hero locale={locale || "es"}/>
+
         </div>
     );
 };
---
+++ components/Home/backgroudAnimate.tsx
@@ -0,0 +1,35 @@
+import {FC} from "react";
+
+const BackgroudAnimate: FC = () => {
+    return (
+        <ul className="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
+
+export default BackgroudAnimate;
