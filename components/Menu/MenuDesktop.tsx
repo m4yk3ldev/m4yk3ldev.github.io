08d5c7e53d99f7b35152717ccb64fd3c1db08f6f
Maykel Arias Torres <hanibal920915@gmail.com>
2023-01-13 00:07:23 -0300

    fix menuDesktop

--- components/Menu/MenuDesktop.tsx
+++ components/Menu/MenuDesktop.tsx
@@ -2,19 +2,18 @@ import { FC, useEffect } from "react";
 import Link from "next/link";
 
 export const MenuDesktop: FC = () => {
-  useEffect(() => {}, []);
   return (
     <>
       <div className="menu pr-3">
         <div className="navigation flex">
           <div className="menuToggle"></div>
           <ul className="flex flex-row gap-2">
-            <li className="flex flex-1 active mouseaction">
+            <li className="active mouseaction flex flex-1">
               <Link href="#Home">
                 <span className="text">Inicio</span>
               </Link>
             </li>
-            <li className="flex flex-1 mouseaction">
+            <li className="mouseaction flex flex-1">
               <Link href="#About">
                 <span className="text">Acerca</span>
               </Link>
