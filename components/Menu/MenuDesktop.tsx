b2edebe65e1a15e71a4f848e377e57ab6c59cafa
Maykel Arias Torres <hanibal920915@gmail.com>
2023-01-29 12:54:48 -0300

    Correcting lenght in select menu

--- components/Menu/MenuDesktop.tsx
+++ components/Menu/MenuDesktop.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -5,10 +6,24 @@ import { defaultLang } from "../../lib/config";
 import { GetLocaleStorage, TypeLocale } from "../../lib/locale";
 import useScrollPosition from "../../lib/hook/usescroll";
 
+interface ILengthSection {
+  top: number;
+  bottom: number;
+  status: string;
+}
+
 export const MenuDesktop: FC = () => {
   const [locale, setLocale] = useState<TypeLocale>(defaultLang);
-  const [statusHome, setStatusHome] = useState<string>("");
-  const [statusAbout, setStatusAbout] = useState<string>("");
+  const [home, setHome] = useState<ILengthSection>({
+    top: 0,
+    bottom: 0,
+    status: "",
+  });
+  const [about, setAbout] = useState<ILengthSection>({
+    top: 0,
+    bottom: 0,
+    status: "",
+  });
   const router = useRouter();
   const [t, setT] = useState(GetLocaleStorage(locale).MenuDesktop);
   useEffect(() => {
@@ -19,37 +34,48 @@ export const MenuDesktop: FC = () => {
   }, [locale]);
 
   const handleClickHome = () => {
-    setStatusAbout("");
-    setStatusHome("active");
+    setAbout({ ...about, status: "" });
+    setHome({ ...home, status: "active" });
   };
   const handleClickAbout = () => {
-    setStatusAbout("active");
-    setStatusHome("");
+    setAbout({ ...about, status: "active" });
+    setHome({ ...home, status: "" });
   };
 
   const position = useScrollPosition();
 
   useEffect(() => {
-    if (position.y <= 100) {
+    const { y } = position;
+    if (y <= home.bottom && y >= home.top) {
       handleClickHome();
-    } else if (position.y > 100) {
+    } else if (y >= about.top && y <= about.bottom) {
       handleClickAbout();
     } else {
       handleClickHome();
     }
   }, [position]);
+  useEffect(() => {
+    const Home = document.getElementById("Home");
+    const About = document.getElementById("About");
+    if (Home) {
+      setHome({ ...home, top: Home.offsetTop, bottom: Home.offsetWidth });
+    }
+    if (About) {
+      setAbout({ ...about, top: About.offsetTop, bottom: About.offsetWidth });
+    }
+  }, []);
   return (
     <>
       <div className="menu">
         <div className="navigation flex">
           <div className="menuToggle"></div>
           <ul className="flex flex-row gap-2">
-            <li className={`mouseaction flex flex-1  ${statusHome} `}>
+            <li className={`mouseaction flex flex-1  ${home.status} `}>
               <Link href="#Home" onClick={handleClickHome}>
                 <span className="text">{t.home}</span>
               </Link>
             </li>
-            <li className={`mouseaction flex flex-1 ${statusAbout}`}>
+            <li className={`mouseaction flex flex-1 ${about.status}`}>
               <Link href="#About" onClick={handleClickAbout}>
                 <span className="text">{t.about}</span>
               </Link>
