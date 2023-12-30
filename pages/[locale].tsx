5a1a419d451b11db6c3c114e01a4b52ee0a5a195
Maykel <hanibal920915@gmail.com>
2023-12-30 02:28:54 +0000

    Refactor import statement in [locale].tsx

--- pages/[locale].tsx
+++ pages/[locale].tsx
@@ -1,14 +1,14 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Home from "../components/Home/Home";
 import MainLayout from "../components/Layout/MainLayout";
-import { useMemo } from "react";
+import { useEffect } from "react";
 import { defaultLang } from "../lib/config";
 
 interface Props {
   locale: string;
 }
 const HomePage: NextPage<Props> = ({ locale }) => {
-  useMemo(() => {
+  useEffect(() => {
     localStorage.setItem("locale", locale);
   }, [locale]);
   return (
